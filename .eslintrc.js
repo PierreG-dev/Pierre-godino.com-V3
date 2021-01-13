5245b50c1df927497f9c1b5fdd772412b0a2d2ea
AniTecK <aniteck82@hotmail.fr>
2021-01-13 13:40:13 +0100

    Configurations for eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     'prettier/@typescript-eslint',
     'react-app',
     'plugin:prettier/recommended',
+    'prettier/prettier',
   ],
   plugins: ['@typescript-eslint', 'react', 'jsx-a11y'],
   rules: {
@@ -12,6 +13,19 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/ban-ts-comment': 0,
+    'react/jsx-wrap-multilines': [
+      'error',
+      {
+        declaration: 'parens-new-line',
+        assignment: 'parens-new-line',
+        return: 'parens-new-line',
+        arrow: 'parens-new-line',
+        arrow: 'parens-new-line',
+        condition: 'parens-new-line',
+        logical: 'parens-new-line',
+        prop: 'parens-new-line',
+      },
+    ],
   },
   overrides: [
     {
