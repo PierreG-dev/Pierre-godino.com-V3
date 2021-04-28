dec8e69d8d3506c36058dfb7a131e7fdf0c4aacd
pierreg-dev <aniteck82@hotmail.fr>
2021-04-28 15:18:50 +0200

    New navbar design

--- src/components/Layout/navbar/Navbar.tsx
+++ src/components/Layout/navbar/Navbar.tsx
@@ -1,43 +1,163 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompass } from '@fortawesome/free-regular-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar: React.FC = () => {
+  const [displayNav, setDisplayNav] = useState(false);
+
   return (
     <MainContainer>
-      <nav className="">
-        <div className="flex justify-center gap-12">
-          <a href="/about/curiculum">CV</a>
-          <a href="/about/experiences">Expériences</a>
-          <a href="/about/technologies">Technologies</a>
-        </div>
+      <nav
+        className={
+          'nav flex justify-center items-center ' +
+          (displayNav ? 'displayed' : 'folded')
+        }
+        onClick={() => {
+          if (displayNav === false) setDisplayNav(true);
+        }}>
+        <FontAwesomeIcon
+          icon={faCompass}
+          className={'' + (displayNav && 'opacity-0')}
+          id="compass"
+        />
+        <ul
+          className={
+            'flex justify-center items-center ' +
+            (displayNav ? 'visible-links' : 'invisible-links')
+          }>
+          {displayNav && (
+            <React.Fragment>
+              <li>
+                <a href="/about/curiculum" className="effect-underline">
+                  CV
+                </a>
+              </li>
+              <li>
+                <a href="/about/experiences" className="effect-underline">
+                  Expériences
+                </a>
+              </li>
+              <li>
+                <a href="/about/technologies" className="effect-underline">
+                  Technologies
+                </a>
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  onClick={() => setDisplayNav(false)}
+                  id="times"
+                />
+              </li>
+            </React.Fragment>
+          )}
+        </ul>
       </nav>
     </MainContainer>
   );
 };
 
 const MainContainer = styled.div`
-  position: absolute;
+  position: fixed;
   width: 100%;
   padding: 1rem;
   transition: 0.2s;
-  color: #fafafa;
-  background: rgba(0, 0, 0, 0.1);
+  color: #373737;
   font-family: BebasNeue;
-  box-shadow: 0 0px 1px rgba(0, 0, 0, 0.12);
-  font-size: 1.7rem;
   z-index: 10;
 
-  a {
-    opacity: 0.7;
-    transition: 0.2s;
+  .displayed {
+    width: 375px;
+    height: 40px;
+    transition: 0.7s 0.8s ease;
   }
 
-  &:hover {
-    background: rgba(40, 40, 40, 0.8);
-    padding: 2rem;
+  .folded {
+    width: 40px;
+    height: 40px;
+    transition: 0.7s ease;
   }
-  &:hover a {
-    opacity: 1;
+  #times {
+    font-size: 1rem;
+    margin-right: -40px;
+  }
+  @media screen and (max-width: 600px) {
+    .displayed {
+      width: 330px;
+      height: 40px;
+      transition: 0.7s 0.8s ease;
+    }
+
+    #times {
+      margin-right: 0;
+    }
+  }
+
+  .nav {
+    background: #fafafa;
+    border-radius: 50px;
+    box-shadow: 0 0px 1px rgba(0, 0, 0, 0.25);
+
+    #compass {
+      cursor: pointer;
+      position: absolute;
+      font-size: 2rem;
+      transition: 0.7s 0.3s;
+    }
+
+    ul {
+      list-style: none;
+      font-size: 1.7rem;
+    }
+    .invisible-links {
+      opacity: 0;
+      transition: 0.8s;
+    }
+    .visible-links {
+      opacity: 1;
+      gap: 15px;
+      transition: 0.8s 1.5s;
+    }
+    ul li {
+      cursor: pointer;
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    &:hover #compass {
+      transform: scale3d(1.1, 1.1, 1);
+    }
+
+    &:active {
+      transform: scale3d(0.9, 0.9, 1);
+    }
+
+    /*effect-underline*/
+    a.effect-underline:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      display: inline-block;
+      height: 1em;
+      width: 100%;
+      border-bottom: 1px solid;
+      margin-top: 10px;
+      opacity: 0;
+      -webkit-transition: opacity 0.35s, -webkit-transform 0.35s;
+      transition: opacity 0.35s, transform 0.35s;
+      -webkit-transform: scale(0, 1);
+      transform: scale(0, 1);
+    }
+
+    a.effect-underline:hover:after {
+      opacity: 1;
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
   }
 `;
 
