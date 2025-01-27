b31fff18ff79da3f5292aefd965cad83c1a53a04
PierreG-dev <aniteck82@hotmail.fr>
2025-01-27 20:06:30 +0100

    removed cors

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ app.use(
       "http://creation-sites-godino.fr/",
       "176.128.76.57:3000",
       "176.128.76.57:*",
+      "*",
     ],
     credentials: true,
     optionsSuccessStatus: 204,
