f9ddd56e677f7d72ef3c8d3ba93ad11bc847c1fa
PierreG-dev <aniteck82@hotmail.fr>
2025-02-08 20:06:51 +0100

    test without gtag

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true', // Assurez-vous que la variable d'environnement est définie sur 'true' pour activer l'analyse
+});
+
+module.exports = withBundleAnalyzer({
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'localhost',
-        port: '3000',
-      },
-      {
-        protocol: 'https',
-        hostname: 'creation-sites-godino.fr',
-      },
-      {
-        protocol: 'https',
-        hostname: 'blog.api.pierre-godino.com',
-      },
+      { protocol: 'https', hostname: 'localhost', port: '3000' },
+      { protocol: 'https', hostname: 'creation-sites-godino.fr' },
+      { protocol: 'https', hostname: 'blog.api.pierre-godino.com' },
     ],
   },
   productionBrowserSourceMaps: false,
@@ -22,19 +15,9 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['icon-library'],
   },
-  env: {},
   trailingSlash: false,
-
-  // Configuration Webpack
   webpack: (config) => {
     config.resolve.alias['./main.css'] = false; // Désactivation de l'importation de main.css
     return config;
   },
-};
-
-// Ajout de l'analyse du bundle
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true', // TRUE pour faire un rapport à chaque build
 });
-
-module.exports = withBundleAnalyzer(nextConfig);
