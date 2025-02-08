ddbc532d3234d4388862b4c256a128702eb78db2
PierreG-dev <aniteck82@hotmail.fr>
2025-02-08 17:29:44 +0100

    FONT optimizations

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -54,8 +54,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
+          {/* ----- GOOGLE ADS ----- */}
           <script
             async
+            defer
             src="https://www.googletagmanager.com/gtag/js?id=AW-16765589576"></script>
           <script
             dangerouslySetInnerHTML={{
@@ -67,6 +69,8 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+
+          {/* ----- FAVICON ----- */}
           <link
             rel="icon"
             type="image/png"
@@ -82,6 +86,14 @@ export default class MyDocument extends Document {
           />
           <meta name="apple-mobile-web-app-title" content="pierre-godino.com" />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+
+          {/* FONTS */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
