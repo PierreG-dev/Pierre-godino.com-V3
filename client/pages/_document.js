a6ac15cd21e94b6dedc7bdd9b0f920f2d0c6b281
PierreG-dev <aniteck82@hotmail.fr>
2025-03-11 10:48:29 +0100

    added GTM - fix

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/style-prop-object */
-/* eslint-disable jsx-a11y/iframe-has-title */
 // import Document, { Html, Head, Main, NextScript } from 'next/document';
 //
 // class MyDocument extends Document {
@@ -24,7 +22,6 @@
 // export default MyDocument;
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import Script from 'next/script';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -57,21 +54,21 @@ export default class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
-          {/* <!-- Google Tag Manager --> */}
-          <Script
-            id="gtm"
-            strategy="afterInteractive"
+          {/* ----- GOOGLE ADS ----- */}
+          <script
+            async
+            defer
+            src="https://www.googletagmanager.com/gtag/js?id=AW-16765589576"></script>
+          <script
             dangerouslySetInnerHTML={{
               __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PVBV69XD');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16765589576');
           `,
             }}
           />
-          {/* <!-- End Google Tag Manager --> */}
 
           {/* ----- FAVICON ----- */}
           <link
@@ -99,15 +96,6 @@ export default class MyDocument extends Document {
           /> */}
         </Head>
         <body>
-          {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-PVBV69XD"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"></iframe>
-          </noscript>
-          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
