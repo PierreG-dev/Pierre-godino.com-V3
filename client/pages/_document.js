b546487f6394cea7a982a6e1226fbf5c9fe1aeb4
PierreG-dev <aniteck82@hotmail.fr>
2025-03-11 10:50:53 +0100

    added GTM - fix

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable react/style-prop-object */
 // import Document, { Html, Head, Main, NextScript } from 'next/document';
 //
 // class MyDocument extends Document {
@@ -62,10 +64,11 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16765589576');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PVBV69XD');
           `,
             }}
           />
@@ -96,6 +99,13 @@ export default class MyDocument extends Document {
           /> */}
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PVBV69XD"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
