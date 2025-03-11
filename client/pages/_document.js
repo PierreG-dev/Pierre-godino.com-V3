8875ed54fcb3c523760a20f64f201c5e32d36bbf
PierreG-dev <aniteck82@hotmail.fr>
2025-03-11 10:39:50 +0100

    added GTM

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -54,21 +54,19 @@ export default class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
-          {/* ----- GOOGLE ADS ----- */}
-          <script
-            async
-            defer
-            src="https://www.googletagmanager.com/gtag/js?id=AW-16765589576"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16765589576');
-          `,
-            }}
-          />
+
+          {/* <!-- Google Tag Manager --> */}
+          <script>(function(w,d,s,l,i) {
+          w[l]=w[l]||[];
+          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),
+          dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PVBV69XD');</script>
+          {/* <!-- End Google Tag Manager --> */}
+
 
           {/* ----- FAVICON ----- */}
           <link
@@ -96,6 +94,10 @@ export default class MyDocument extends Document {
           /> */}
         </Head>
         <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVBV69XD"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+{/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
